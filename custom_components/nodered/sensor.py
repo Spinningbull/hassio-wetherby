2d881e9deb4077504574ddcb4fe3e3c5866b1639
spinningbull <todd@hayfamily.com>
2022-04-09 13:41:10 -0500

    Updates from UI

--- custom_components/nodered/sensor.py
+++ custom_components/nodered/sensor.py
@@ -1,17 +1,16 @@
 """Sensor platform for nodered."""
-from datetime import date, datetime
+from datetime import datetime
 import logging
 from typing import Optional, Union
 
 from dateutil import parser
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorDeviceClass, SensorEntity
 from homeassistant.const import CONF_STATE, CONF_UNIT_OF_MEASUREMENT
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
-from homeassistant.helpers.typing import StateType
 
 from . import NodeRedEntity
 from .const import (
-    CONF_ATTRIBUTES,
+    CONF_CONFIG,
     CONF_LAST_RESET,
     CONF_SENSOR,
     CONF_STATE_CLASS,
@@ -47,28 +46,49 @@ class NodeRedSensor(NodeRedEntity, SensorEntity):
     def __init__(self, hass, config):
         """Initialize the sensor."""
         super().__init__(hass, config)
-        self._state = config.get(CONF_STATE)
-        self.attr = config.get(CONF_ATTRIBUTES, {})
-
-    @property
-    def native_value(self) -> Union[StateType, date, datetime]:
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def native_unit_of_measurement(self) -> Union[str, None]:
-        """Return the unit of measurement of this entity, if any."""
-        return self._config.get(CONF_UNIT_OF_MEASUREMENT)
-
-    @property
-    def state_class(self) -> Optional[str]:
-        """Return the state class."""
-        return self._config.get(CONF_STATE_CLASS)
+        self._attr_unit_of_measurement = None
+        self._attr_native_value = self.convert_state(config.get(CONF_STATE))
+        self._attr_native_unit_of_measurement = self._config.get(
+            CONF_UNIT_OF_MEASUREMENT
+        )
+        self._attr_state_class = self._config.get(CONF_STATE_CLASS)
 
     @property
     def last_reset(self) -> Optional[datetime]:
         """Return the last reset."""
-        try:
-            return parser.parse(self._config.get(CONF_LAST_RESET))
-        except (ValueError, TypeError):
-            return None
+        reset = self._config.get(CONF_LAST_RESET)
+        if reset is not None:
+            try:
+                return parser.parse(reset)
+            except (ValueError, TypeError):
+                _LOGGER.error(
+                    f"Invalid ISO date string ({reset}): {self.entity_id} requires last_reset to be an iso date formatted string"
+                )
+
+        return None
+
+    def convert_state(self, state) -> Union[datetime, float, int, str, bool]:
+        """Convert state if needed."""
+        if state is not None and self.device_class == SensorDeviceClass.TIMESTAMP:
+            try:
+                return parser.parse(state)
+            except (ValueError, TypeError):
+                _LOGGER.error(
+                    f"Invalid ISO date string ({state}): {self.entity_id} has a timestamp device class"
+                )
+                return None
+
+        return state
+
+    def update_entity_state_attributes(self, msg):
+        """Update entity state attributes."""
+        super().update_entity_state_attributes(msg)
+        self._attr_native_value = self.convert_state(msg.get(CONF_STATE))
+
+    def update_discovery_config(self, msg):
+        """Update entity config."""
+        super().update_discovery_config(msg)
+        self._attr_native_unit_of_measurement = msg[CONF_CONFIG].get(
+            CONF_UNIT_OF_MEASUREMENT
+        )
+        self._attr_unit_of_measurement = None
